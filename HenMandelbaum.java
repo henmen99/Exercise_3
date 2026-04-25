631b6bd25429393b1ac9a3ebb168fed7582127a8
henmen99 <henmendelbaum@gmail.com>
2026-04-26 01:13:09 +0300

    Initial commit with Hello and HenMandelbaum files

---
+++ HenMandelbaum.java
@@ -0,0 +1,10 @@
+public class HenMandelbaum
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 12; i++)
+        {
+            System.out.println("Hen Mandelbaum" + i);
+        }
+    }
+}
